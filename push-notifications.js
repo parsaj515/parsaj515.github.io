ebd6cac741603fc652fe4a9dfee22727138ec7c3
Parsa <jparsa515@gmail.com>
2025-11-12 10:00:48 -0800

    Update push-notifications.js

--- push-notifications.js
+++ push-notifications.js
@@ -1,1 +1,7 @@
-self.addEventListener('push',function(e){const options={body:e.data.text(),icon:'IMG_0054.ico',badge:'IMG_0054.ico'};e.waitUntil(self.registration.showNotification('New Message',options))});
+self.addEventListener('push', function(event) {
+  const data = event.data.json();
+  self.registration.showNotification(data.title, {
+    body: data.body,
+    icon: 'IMG_0054.ico'
+  });
+});
