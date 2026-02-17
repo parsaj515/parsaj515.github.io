88f0f40e97d90c679936c5867b7dfd4e6b27d0ff
Parsa <jparsa515@gmail.com>
2026-02-17 13:26:29 -0800

    Delete scripts directory

--- scripts/theme.js
+++
@@ -1,6 +0,0 @@
-const savedTheme = localStorage.getItem('theme');
-if (savedTheme) {
-	document.documentElement.setAttribute('data-theme', savedTheme);
-} else {
-	document.documentElement.setAttribute('data-theme', 'light');
-}
